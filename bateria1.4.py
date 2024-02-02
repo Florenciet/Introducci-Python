4f788aa19a8514869e8c09f339308d9627a7fa5e
Florenciet <158280986+Florenciet@users.noreply.github.com>
2024-02-02 14:04:05 +0100

    Create bateria1.4.py

---
+++ bateria1.4.py
@@ -0,0 +1,5 @@
+#Programa que demana un pes en kg a l'usuari i retorna el mateix pes convertit en lliures.
+
+pes = int(input("Introdueix el teu pes: "))
+resultat = pes*2.20462
+print("El teu pes en lliures és: ",resultat)
