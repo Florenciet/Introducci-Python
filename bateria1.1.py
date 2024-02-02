d71f682d7cfc8ad47b3f33e9f2353ff298fdcfd6
Florenciet <158280986+Florenciet@users.noreply.github.com>
2024-02-02 13:35:19 +0100

    Create bateria1.1.py

---
+++ bateria1.1.py
@@ -0,0 +1,7 @@
+# Programa que pregunta dos números a l'usuari i calcula la seva suma.
+
+num1 = input("Introdueix un número: ")
+num2 = input("Introdueix el següent nombre: ")
+resultat = int(num1) + int(num2)
+
+print("El resultat és: ",resultat)
